908580904dac6a8940cc751793ec739c7a8dc361
Lenny <valentin805@gmail.com>
2018-08-01 19:54:56 +0300

    Add Teachers rest services

--- Server/src/main/java/academy/areas/teachers/repositories/TeacherRepository.java
+++ Server/src/main/java/academy/areas/teachers/repositories/TeacherRepository.java
@@ -4,7 +4,11 @@ import academy.areas.teachers.entities.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher,Integer> {
-
+    List<Teacher> findAllByActiveTrue();
+    Teacher findTeacherByIdAndActiveTrue(final Integer id);
+    Teacher findTeacherByUserNameAndActiveTrue(final String userName);
 }
--- Server/src/main/java/academy/areas/teachers/services/TeacherServicesImpl.java
+++ Server/src/main/java/academy/areas/teachers/services/TeacherServicesImpl.java
@@ -1,8 +1,12 @@
 package academy.areas.teachers.services;
 
+import academy.areas.students.entities.Student;
+import academy.areas.teachers.entities.Teacher;
 import academy.areas.teachers.repositories.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TeacherServicesImpl implements TeacherServices {
@@ -13,4 +17,41 @@ public class TeacherServicesImpl implements TeacherServices {
         this.teacherRepository = teacherRepository;
     }
 
+    @Override
+    public Integer createTeacher(Teacher teacher) {
+        if (teacher != null){
+            this.teacherRepository.saveAndFlush(teacher);
+            return this.teacherRepository
+                    .findTeacherByUserNameAndActiveTrue(teacher.getUserName())
+                    .getId();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Teacher> getAllTeachers() {
+        return this.teacherRepository.findAllByActiveTrue();
+    }
+
+    @Override
+    public Teacher getTeacherById(Integer id) {
+        return this.teacherRepository.findTeacherByIdAndActiveTrue(id);
+    }
+
+    @Override
+    public void disableTeacher(Integer id) {
+        Teacher teacher = this.teacherRepository.findTeacherByIdAndActiveTrue(id);
+        if (teacher != null){
+            teacher.setActive(false);
+            this.teacherRepository.saveAndFlush(teacher);
+        }
+    }
+
+    @Override
+    public void updateTeacher(Teacher teacher) {
+        if (teacher != null && this.teacherRepository
+                .findTeacherByIdAndActiveTrue(teacher.getId()) != null){
+            this.teacherRepository.saveAndFlush(teacher);
+        }
+    }
 }
