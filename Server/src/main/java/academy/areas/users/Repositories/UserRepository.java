0eb3a503af3f00d646bef9ce22870a3b72f45d12
Lenny <valentin805@gmail.com>
2018-08-03 12:07:15 +0300

    Add user and role repositories

---
+++ Server/src/main/java/academy/areas/users/Repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package academy.areas.users.Repositories;
+
+import academy.areas.users.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+}
